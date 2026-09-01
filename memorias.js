ada5505eaff35c54e6b1c7b97390b4b09e87d920
ghost <138735754+Ryan20014737472@users.noreply.github.com>
2026-09-01 10:20:58 -0300

    Add celebration entry for {ROBOCEP} victory
    Added a new entry for {ROBOCEP} celebrating their victory, including title, text, image, and audio.

--- memorias.js
+++ memorias.js
@@ -671,7 +671,22 @@ const MEMORIAS = [
       "mime": "audio/mpeg"
     }
   },
-   {
+ {
+    "titulo": "{ROBOCEP} comemorando",
+    "texto": "essa foto mostra toda a equipe {ROBOCEP} comemorando após ficar em 1° lugar nas duas categorias da {TBR}",
+    "corrigirOrtografia": true,
+    "midia": {
+      "tipo": "imagem",
+      "arquivo": "assets/images/equipe inteir.jpg",
+      "alt": "equiper"
+    },
+    "audio": {
+      "arquivo": "assets/audio/winni.mp3",
+      "mime": "audio/mpeg"
+    }
+  }, 
+  
+  {
     "titulo": "Uber para habibs",
     "texto": "Eu, Leninha e Leleo indo pro Habibs de Uber pós vitória TBR.",
     "midia": {
